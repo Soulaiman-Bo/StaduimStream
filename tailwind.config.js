b0b4ca5e726b9bfc3a69da759c1b4177f1bc6bbb
meed ghoufrane <baloncesmeed@gmail.com>
2023-12-29 11:57:14 +0100

    i integrated sign in and sign up pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js}", 
             "./views/home/*.php",
             "./views/*.php",
+            "./views/user/*.php",
           ],
   theme: {
     extend: {},
