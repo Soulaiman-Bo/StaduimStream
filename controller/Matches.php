9f5f43c48bec565108b18521905d51a20c638b0c
bwissal <baazizwissal13@gmail.com>
2024-01-02 15:40:59 +0100

    add match add crud

--- controller/Matches.php
+++ controller/Matches.php
@@ -14,5 +14,35 @@ class Matches extends Controller
         require_once "$view";
     }
 
+    protected function add()
+    {
+        $view = $this->getView();
+        require_once "$view";
+    }
+    protected function addaction(){
+        extract($_POST);
+        $view = new MatchesModel();
+        $matchfields = array( 
+            'date'=> $date,
+            'Stadium'=> $Stadium,
+            'team_1'=> $team_name1,
+            'team_2'=>$team_name2,
+           
+        );
+        $insertedId = $view->insertRecord("matche", $matchfields);
+        $view->closeConnection();
+
+		if ($insertedId) {
+			$message = "Match inserted successfully!";
+			http_response_code(200);
+			echo json_encode([
+				"message" => $message,
+				"Id" => $insertedId
+			]);
+		}
+
+
+    }
+	
 
 }
