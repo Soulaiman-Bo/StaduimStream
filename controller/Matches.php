18f34d0a96eddbac1036267e6705fde8b9a8faed
bwissal <baazizwissal13@gmail.com>
2024-01-03 11:24:14 +0100

    add delete feauture match crud

--- controller/Matches.php
+++ controller/Matches.php
@@ -19,6 +19,29 @@ class Matches extends Controller
         $view = $this->getView();
         require_once "$view";
     }
+    protected function delete()
+    {
+       
+		$id = $_GET['id'];
+
+		if ($id !== "") {
+			$matchmodel = new MatchesModel();
+			$row = $matchmodel->selectSingleRecords("matche", "*", "id = $id");
+			$view = $this->getView($matchmodel->Index(), false);
+			require_once "$view";
+		} else {
+			echo '<h1>ERROR 404: Bad Request</h1>';
+		}
+	}
+	
+
+    protected function show()
+    {
+        $viewmodel = new MatchesModel();
+        $rows = $viewmodel->selectJoin("matche");
+        $view = $this->getView();
+        require_once "$view";
+    }
     protected function addaction(){
         extract($_POST);
         $view = new MatchesModel();
@@ -43,6 +66,67 @@ class Matches extends Controller
 
 
     }
-	
+	protected function deleteaction()
+	{
+		extract($_POST);
 
+		$viewmodel = new MatchesModel();
+
+		$id = $_POST['id'];
+
+		$result = $viewmodel->deleteRecord("matche", "id", $id);
+
+		if ($result) {
+			$message = "Match deleted successfully!";
+			http_response_code(200);
+			echo json_encode([
+				"message" => $message,
+				"Id" => $result
+			]);
+		}
+	}
+	protected function update()
+	{
+		$id = $_GET['id'];
+
+		if ($id !== "") {
+			$viewmodel = new MatchesModel();
+			$rows = $viewmodel->selectSingleRecords("matche", "*", "id = $id");
+
+			if ($rows) {
+				$view = $this->getView();
+				require_once "$view";
+			} else {
+				echo "<h1>ERROR 404: Bad Request</h1>";
+			}
+		} else {
+			echo '<h1>ERROR 404: Page Not Found</h1>';
+		}
+	}
+    protected function updateaction()
+	{
+		extract($_POST);
+		$viewmodel = new MatchesModel();
+
+		$id = $_POST['id'];
+
+		$matchfields = array(	
+			'date'=> $date,
+            'Stadium'=> $Stadium,
+            'team_1'=> $team_name1,
+            'team_2'=>$team_name2,
+		);
+
+		$insertedId = $viewmodel->updateRecord("matche", $matchfields, "id", $id);
+		$viewmodel->closeConnection();
+
+		if ($insertedId) {
+			$message = "Match Updated successfully!";
+			http_response_code(200);
+			echo json_encode([
+				"message" => $message,
+				"Id" => $insertedId
+			]);
+		}
+	}
 }
