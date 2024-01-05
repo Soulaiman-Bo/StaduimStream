d87ccf6fb6ed6bfd3936330cf5d0698721ec60c7
meed ghoufrane <baloncesmeed@gmail.com>
2024-01-05 09:39:09 +0100

    edit dashborad

--- views/includes/sidebar.php
+++ views/includes/sidebar.php
@@ -3,7 +3,7 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html "
+        <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard "
             target="_blank">
             <span class="ms-1 font-weight-bold">Dashboard</span>
         </a>
@@ -12,7 +12,7 @@
     <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="http://staduimstream.test/">
+                <a class="nav-link" href="http://staduimstream.test/Dashboard">
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
